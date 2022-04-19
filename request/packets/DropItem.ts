39963c6f25195cb1200ac9b2453de8caab355faa
Anthony <29609366+anthony-hyo@users.noreply.github.com>
2022-04-18 23:07:18 -0300

    Drop item request

---
+++ request/packets/DropItem.ts
@@ -0,0 +1,12 @@
+import Bot from "../../bot/Bot";
+import IRequest from "../../interface/IRequest";
+
+export default class DropItem implements IRequest {
+
+    public command: string = 'dropItem'
+
+    handler(bot: Bot, data: any): void {
+        bot.handler.onDropItem()
+    }
+
+}
